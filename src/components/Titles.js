5d0d1c32cadcf5a797cb710c15d00fea68f7502a
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-10-11 14:37:17 -0400

    change Titles to stateless component

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-const Titles = props => (
+const Titles = () => (
   <div>
     <h1>Weather Finder</h1>
     <p>Find out temperature, conditions, and more...</p>
